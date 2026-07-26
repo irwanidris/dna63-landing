f4d6fb46eccda779b5b81c3aead35ff44b7c0d7b
Irwan Idris <irwanyzan@gmail.com>
2026-07-26 22:17:42 +0800

    Fix 404 by adding rewrites to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/privacy.html',
+        destination: '/privacy',
+      },
+      {
+        source: '/delete-account.html',
+        destination: '/delete-account',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
